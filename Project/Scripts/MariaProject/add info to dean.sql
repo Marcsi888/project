27f114dece9707525b6c30c23f59b2dcaae04c36
John Doe <nyolcas.marcsi@gmail.com>
2024-04-02 16:22:34 +0200

    changes

---
+++ Project/Scripts/MariaProject/add info to dean.sql
@@ -0,0 +1,3 @@
+INSERT INTO dean (DeanID, FirstName, LastName, BirthDate, DeanEmail)
+VALUES (1, 'Jose', 'Martin','1964-06-18' ,'jose.martin@scuniversity.com'),
+       (2, 'Evelin', 'Smith','1992-08-23', 'evelin.smith@scuniversity.com');
